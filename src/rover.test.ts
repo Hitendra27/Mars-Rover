e1332186969274cb9ccfbf09740247889be373e2
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-12 23:06:52 +0100

    feat: Implemented TDD mars rover turn Right

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,11 +1,22 @@
-import { marsRover } from "./rover";
+//import { marsRover } from "./rover";
+//import { turnLeft } from "./rover"; 
 
-const turnLeft = (X: string) => {
-    if (X === 'N') return 'W';
-    return 'S';
+export const turnLeft = (X: string) => {
+    switch (X) {
+        case "N":
+          return "W";
+        case "W":
+          return "S";
+        case "S":
+          return "E";
+        case "E":
+          return "N";
+    }
+
 }
 
-describe('mars rover tests', () => {
+
+describe('mars rover turn Left tests', () => {
     test('When facing N, turning left should cause us to face W', () => {
         // Arrange
         const expected = turnLeft('N');
@@ -23,6 +34,75 @@ describe('mars rover tests', () => {
 
         // Act
         const result = 'S';
+
+        // Assert
+        expect(result).toBe(expected);
+    });
+
+    test('When face S, turning left should cause us to face E', () => {
+        // Arrange
+        const expected = turnLeft('S');
+
+        // Act
+        const result = 'E';
+
+        // Assert
+        expect(result).toBe(expected);
+    });
+
+    test('When face E, turning left should cause us to face N', () => {
+        // Arrange
+        const expected = turnLeft('E');
+
+        // Act
+        const result = 'N'
+
+        // Assert
+        expect(result).toBe(expected);
+    })
+
+});
+
+describe('mars rover turn Right tests', () => {
+    test('When facing N, turning left should cause us to face W', () => {
+        // Arrange
+        const expected = turnLeft('N');
+
+        // Act
+        const result = 'E';
+
+        // Assert
+        expect(result).toBe(expected);
+    });
+
+    test('When facing W, turning left should cause us to face S', () => {
+        // Arrange
+        const expected = turnLeft('E');
+
+        // Act
+        const result = 'S';
+
+        // Assert
+        expect(result).toBe(expected);
+    });
+
+    test('When face S, turning left should cause us to face E', () => {
+        // Arrange
+        const expected = turnLeft('S');
+
+        // Act
+        const result = 'W';
+
+        // Assert
+        expect(result).toBe(expected);
+    });
+
+    test('When face E, turning left should cause us to face N', () => {
+        // Arrange
+        const expected = turnLeft('W');
+
+        // Act
+        const result = 'N'
 
         // Assert
         expect(result).toBe(expected);
--- src/rover.ts
+++ src/rover.ts
@@ -1,3 +1,15 @@
-export const marsRover = () => {
-    
+//export const marsRover = () => {}
+
+export const turnLeft = (X: string) => {
+    switch (X) {
+        case "N":
+          return "W";
+        case "W":
+          return "S";
+        case "S":
+          return "E";
+        case "E":
+          return "N";
+    }
+
 }
