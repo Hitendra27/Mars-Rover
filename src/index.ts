e59ffdb6ef9a47bd7946e2b71d192c362a668642
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-06-14 20:08:26 +0100

    feat: Implemented index.ts file

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import readline from 'readline';
+//import Rover from './rover';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+export const stratRover = (): void =>  {
+    rl.question('Enter the initial position and direction of the rover (e.g., 0 0 N): ', (initialPosition) => {
+        const [x, y, direction] = initialPosition.split(' ');
+        const rover = new Rover(parseInt(x), parseInt(y), direction);
+
+        rl.question('Enter the sequence of commands for the rover: ', (commands) => {
+            const finalPosition = rover.move(commands);
+            console.log('Final position: ${finalPosition}');
+
+            rl.question('Do you want to run another rover? (y/n: ', (answer) => {
+                if (answer.toLowerCase() === 'y') {
+                    stratRover();
+                } else {
+                    rl.close();
+                }
+            });
+        });
+    });
+}
+
+console.log('=== Welcome to the Mars Rover ===');
+stratRover();
